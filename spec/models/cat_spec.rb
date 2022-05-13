d810f7c49575e0ff07537498b9863e814003cf07
WalkerTexas-coder <austinsrwalker@gmail.com>
2022-05-13 11:14:21 -0700

    validations lecture, enjoys length

--- spec/models/cat_spec.rb
+++ spec/models/cat_spec.rb
@@ -25,6 +25,11 @@ RSpec.describe Cat, type: :model do
       expect(cat.errors[:image]).to_not be_empty
       # expect the Cat object to have a key called Errors that has a nested key by the name image. Errors.image should have some kind of response that let's me know it is not allowed. 
     end
+    it "enjoys must have minimum length of ten" do
+      cat = Cat.create name:'Toby Keith', age:42,enjoys:'surfing', image:'https://wp.usatodaysports.com/wp-content/uploads/sites/88/2020/06/screen-shot-2020-06-03-at-9.47.38-am.jpg'
+     
+      expect(cat.errors[:enjoys]).to_not be_empty
+    end
   end
     
 end

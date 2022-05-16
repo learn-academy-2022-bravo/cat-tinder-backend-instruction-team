2651f4c7b0931214d09022a6abee040f721b2171
Sarah Proctor <sproctor950@gmail.com>
2022-05-16 12:14:13 -0700

    adds update validations

--- spec/requests/cats_spec.rb
+++ spec/requests/cats_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
-RSpec.describe "Cats", type: :request do
-  describe "GET /index" do
+RSpec.describe 'Cats', type: :request do
+  describe 'GET /index' do
     it 'return a list of all cats' do
       Cat.create(
         name: 'Toast',
@@ -16,7 +16,7 @@ RSpec.describe "Cats", type: :request do
     end
   end
 
-  describe "POST /create" do
+  describe 'POST /create' do
     it 'can create a cat' do
       cat_params = {
         cat: {
@@ -36,7 +36,7 @@ RSpec.describe "Cats", type: :request do
     end
   end
 
-  describe "PATCH /update" do
+  describe 'PATCH /update' do
     it 'can update an existing cat' do
       cat_params = {
         cat: {
@@ -49,7 +49,7 @@ RSpec.describe "Cats", type: :request do
       post '/cats', params: cat_params
       cat = Cat.first
 
-      update_cat_params = {
+      updated_cat_params = {
         cat: {
           name: 'Toast',
           age: 3,
@@ -57,14 +57,14 @@ RSpec.describe "Cats", type: :request do
           image: 'http://www.catpics.com'
         }
       }
-      patch "/cats/#{cat.id}", params: update_cat_params
+      patch "/cats/#{cat.id}", params: updated_cat_params
       updated_cat = Cat.find(cat.id)
       expect(response).to have_http_status(200)
       expect(updated_cat.age).to eq(3)
     end
   end
 
-  describe "DELETE /destroy" do
+  describe 'DELETE /destroy' do
     it 'can delete an existing cat' do
       cat_params = {
         cat: {
@@ -83,7 +83,7 @@ RSpec.describe "Cats", type: :request do
     end
   end
 
-  describe "Cat create request validations" do
+  describe 'create request validations' do
     it "doesn't create a cat without a name" do
       cat_params = {
         cat: {
@@ -93,9 +93,9 @@ RSpec.describe "Cats", type: :request do
         }
       }
       post '/cats', params: cat_params
-      json = JSON.parse(response.body)
+      cat_response = JSON.parse(response.body)
       expect(response).to have_http_status(422)
-      expect(json['name']).to include "can't be blank"
+      expect(cat_response['name'].first).to eq("can't be blank")
     end
 
     it "doesn't create a cat without an age" do
@@ -107,9 +107,9 @@ RSpec.describe "Cats", type: :request do
         }
       }
       post '/cats', params: cat_params
-      json = JSON.parse(response.body)
+      cat_response = JSON.parse(response.body)
       expect(response).to have_http_status(422)
-      expect(json['age']).to include "can't be blank"
+      expect(cat_response['age'].first).to eq("can't be blank")
     end
 
     it "doesn't create a cat without an enjoys" do
@@ -121,12 +121,12 @@ RSpec.describe "Cats", type: :request do
         }
       }
       post '/cats', params: cat_params
-      json = JSON.parse(response.body)
+      cat_response = JSON.parse(response.body)
       expect(response).to have_http_status(422)
-      expect(json['enjoys']).to include "can't be blank"
+      expect(cat_response['enjoys'].first).to eq("can't be blank")
     end
 
-    it "doesn't create a cat without a image" do
+    it "doesn't create a cat without an image" do
       cat_params = {
         cat: {
           name: 'Toast',
@@ -135,10 +135,141 @@ RSpec.describe "Cats", type: :request do
         }
       }
       post '/cats', params: cat_params
-      json = JSON.parse(response.body)
+      cat_response = JSON.parse(response.body)
       expect(response).to have_http_status(422)
-      expect(json['image']).to include "can't be blank"
+      expect(cat_response['image'].first).to eq("can't be blank")
     end
   end
 
+  describe 'update request validations' do
+    it "doesn't update a cat without a name" do
+      cat_params = {
+        cat: {
+          name: 'Toast',
+          age: 2,
+          enjoys: 'allll the attention',
+          image: 'http://www.catpics.com'
+        }
+      }
+      post '/cats', params: cat_params
+      cat = Cat.first
+
+      updated_cat_params = {
+        cat: {
+          name: '',
+          age: 2,
+          enjoys: 'allll the attention',
+          image: 'http://www.catpics.com'
+        }
+      }
+      patch "/cats/#{cat.id}", params: updated_cat_params
+      updated_cat_response = JSON.parse(response.body)
+      expect(response).to have_http_status(422)
+      expect(updated_cat_response['name'].first).to eq("can't be blank")
+    end
+
+    it "doesn't update a cat without an age" do
+      cat_params = {
+        cat: {
+          name: 'Toast',
+          age: 2,
+          enjoys: 'allll the attention',
+          image: 'http://www.catpics.com'
+        }
+      }
+      post '/cats', params: cat_params
+      cat = Cat.first
+
+      updated_cat_params = {
+        cat: {
+          name: 'Toast',
+          age: '',
+          enjoys: 'allll the attention',
+          image: 'http://www.catpics.com'
+        }
+      }
+      patch "/cats/#{cat.id}", params: updated_cat_params
+      updated_cat_response = JSON.parse(response.body)
+      expect(response).to have_http_status(422)
+      expect(updated_cat_response['age'].first).to eq("can't be blank")
+    end
+
+    it "doesn't update a cat without an enjoys" do
+      cat_params = {
+        cat: {
+          name: 'Toast',
+          age: 2,
+          enjoys: 'allll the attention',
+          image: 'http://www.catpics.com'
+        }
+      }
+      post '/cats', params: cat_params
+      cat = Cat.first
+
+      updated_cat_params = {
+        cat: {
+          name: 'Toast',
+          age: 2,
+          enjoys: '',
+          image: 'http://www.catpics.com'
+        }
+      }
+      patch "/cats/#{cat.id}", params: updated_cat_params
+      updated_cat_response = JSON.parse(response.body)
+      expect(response).to have_http_status(422)
+      expect(updated_cat_response['enjoys'].first).to eq("can't be blank")
+    end
+
+    it "doesn't update a cat with an enjoys that is less than 10 characters" do
+      cat_params = {
+        cat: {
+          name: 'Toast',
+          age: 2,
+          enjoys: 'allll the attention',
+          image: 'http://www.catpics.com'
+        }
+      }
+      post '/cats', params: cat_params
+      cat = Cat.first
+
+      updated_cat_params = {
+        cat: {
+          name: 'Toast',
+          age: 2,
+          enjoys: 'sleep',
+          image: 'http://www.catpics.com'
+        }
+      }
+      patch "/cats/#{cat.id}", params: updated_cat_params
+      updated_cat_response = JSON.parse(response.body)
+      expect(response).to have_http_status(422)
+      expect(updated_cat_response['enjoys'].first).to eq("is too short (minimum is 10 characters)")
+    end
+
+    it "doesn't update a cat without an image" do
+      cat_params = {
+        cat: {
+          name: 'Toast',
+          age: 2,
+          enjoys: 'allll the attention',
+          image: 'http://www.catpics.com'
+        }
+      }
+      post '/cats', params: cat_params
+      cat = Cat.first
+
+      updated_cat_params = {
+        cat: {
+          name: 'Toast',
+          age: 2,
+          enjoys: 'allll the attention',
+          image: ''
+        }
+      }
+      patch "/cats/#{cat.id}", params: updated_cat_params
+      updated_cat_response = JSON.parse(response.body)
+      expect(response).to have_http_status(422)
+      expect(updated_cat_response['image'].first).to eq("can't be blank")
+    end
+  end
 end
